fd72d80bdca25fb22f3f2f6f7dbc33315eb66933
AlekseyLevko <AlekseyLevko@gmail.com>
2022-01-22 12:48:31 +0300

    feat: add admin user

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { createConnection } from 'typeorm';
 import app from './app';
+import { addAdminUser } from './common/addAdminUser';
 import { config } from './common/config';
 import { logger } from './logger';
 
 const { PORT } = config;
 
 createConnection().then(() => {
+  addAdminUser();
   try {
     app.listen(PORT, '0.0.0.0', () => {
       logger.info(`App is running on http://localhost:${PORT}`);
