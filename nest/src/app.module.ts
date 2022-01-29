64b6d65bb9b5a239007d75ac729ef3f53a0c31f8
AlekseyLevko <AlekseyLevko@gmail.com>
2022-01-29 15:25:18 +0300

    feat: create typeORM connection

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardsController } from './boards/boards.controller';
+import { Board } from './boards/boards.entity';
+import { config } from './common/config';
+import { TasksController } from './tasks/tasks.controller';
+import { Task } from './tasks/tasks.entity';
+import { UsersController } from './users/users.controller';
+import { User } from './users/users.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: config.POSTGRES_PORT,
+      username: 'postgres',
+      password: 'secretPassword123',
+      database: 'postgres',
+      entities: [User, Board, Task],
+      synchronize: true,
+    }),
+  ],
+  controllers: [
+    AppController,
+    UsersController,
+    TasksController,
+    BoardsController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ nest/src/common/config.ts
@@ -0,0 +1,17 @@
+export const config = {
+  PORT: process.env.PORT || 4000,
+  NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  AUTH_MODE: process.env.AUTH_MODE === 'true',
+  LOGGING_LEVEL: process.env.LOGGING_LEVEL || 'info',
+  DB_TYPE: process.env.DB_TYPE || 'postgres',
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_HOST: process.env.POSTGRES_HOST || 'localhost',
+  POSTGRES_USERNAME: process.env.POSTGRES_USERNAME || 'postgres',
+  POSTGRES_PORT: Number(process.env.POSTGRES_PORT),
+  POSTGRES_SYNCHRONIZE: process.env.POSTGRES_SYNCHRONIZE === 'true',
+  POSTGRES_LOGGING: process.env.POSTGRES_LOGGING === 'true',
+  POSTGRES_MIGRATIONS_RUN: process.env.POSTGRES_MIGRATIONS_RUN === 'true',
+  SALT_ROUNDS: Number(process.env.SALT_ROUNDS) || 10,
+  SECRET_KEY: process.env.SECRET_KEY || '',
+};
