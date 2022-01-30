4e689e22469418df975cfdc69c94c28f51823499
AlekseyLevko <AlekseyLevko@gmail.com>
2022-01-30 11:16:15 +0300

    feat: add some users routes

--- nest/src/users/users.controller.ts
+++ nest/src/users/users.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './create-user.dto';
+import { User } from './users.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
   @Get()
-  findAll(): Promise<any> {
+  findAll(): Promise<User[]> {
     return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
   }
 }
--- nest/src/users/users.service.ts
+++ nest/src/users/users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { CreateUserDto } from './create-user.dto';
 import { User } from './users.entity';
 
 @Injectable()
@@ -12,5 +14,21 @@ export class UsersService {
 
   findAll(): Promise<User[]> {
     return this.usersRepository.find();
+  }
+
+  findOne(id: string): Promise<User> {
+    return this.usersRepository.findOne(id);
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.usersRepository.create({
+      ...createUserDto,
+      id: uuid(),
+    });
+
+    const userToResponse = await this.usersRepository.save(newUser);
+    delete userToResponse.password;
+
+    return userToResponse;
   }
 }
